a423b46af47c43b527b4731157e407de9ec98527
Joseph Lander <josephclander@gmail.com>
2023-12-09 21:23:05 +0000

    Convert to react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,67 @@
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+// the 'types' for
+type InfoState = {
+  fullName: string;
+  day: number;
+  month: string;
+};
+
+const App: React.FC = () => {
+  const [info, setInfo] = useState<InfoState>({
+    fullName: '',
+    day: 0,
+    month: '',
+  });
+
+  const changeHandler = (
+    // a react change event: either input or select coming back
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    setInfo((prevValue) => ({
+      ...prevValue,
+      // day could be empty string or NaN
+      [name]: name === 'day' ? parseInt(value) || 0 : value,
+    }));
+  };
+
+  // this is from a form event returning a form element
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+    console.log(info);
+  };
+
   return (
     <div className="App">
       <h1>Welcome!</h1>
-      <form action="">
+      <form onSubmit={submitHandler}>
         <h2>Who are you?</h2>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <label htmlFor="fullName">Full Name</label>
+        <input
+          type="text"
+          name="fullName"
+          id="fullName"
+          value={info.fullName}
+          onChange={(event) => changeHandler(event)}
+        />
         <h2>When is your birthday?</h2>
         <label htmlFor="day">Day</label>
-        <input type="number" name="day" id="day" />
+        <input
+          type="number"
+          name="day"
+          id="day"
+          value={info.day}
+          onChange={(event) => changeHandler(event)}
+        />
         <label htmlFor="name">Month</label>
-        <select name="month" id="month">
+        <select
+          name="month"
+          id="month"
+          value={info.month}
+          onChange={(event) => changeHandler(event)}
+        >
           <option value="january">January</option>
           <option value="february">February</option>
           <option value="march">March</option>
@@ -26,10 +75,12 @@ function App() {
           <option value="november">November</option>
           <option value="december">December</option>
         </select>
-        <input type="submit" value="Go!" className='submit'/>
+        <button type="submit" className="submit">
+          Go!
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default App;
